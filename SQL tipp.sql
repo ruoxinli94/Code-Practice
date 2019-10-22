a60bcbe1622e6cd95cf019b3a489ef3b9f698030
ruoxinli94 <ruoxinl2@illinois.edu>
2019-10-22 13:27:51 -0500

    Update and rename SQL tips/windowfunction.sql to SQL tipp.sql

---
+++ SQL tipp.sql
@@ -0,0 +1,77 @@
+DECLARE @Version TABLE
+(
+    a int,
+    b int,
+    c int,
+    d int
+)
+ 
+INSERT INTO @Version(a, b, c, d)
+    SELECT 1,1,1,23
+INSERT INTO @Version(a, b, c, d)
+    SELECT 1,2,1,1
+INSERT INTO @Version(a, b, c, d)
+    SELECT 1,2,2,4
+INSERT INTO @Version(a, b, c, d)
+    SELECT 2,1,1,1
+INSERT INTO @Version(a, b, c, d)
+    SELECT 3,1,1,2
+INSERT INTO @Version(a, b, c, d)
+    SELECT 2,1,2,8
+INSERT INTO @Version(a, b, c, d)
+    SELECT 2,1,3,4
+INSERT INTO @Version(a, b, c, d)
+    SELECT 2,2,1,2
+INSERT INTO @Version(a, b, c, d)
+    SELECT 2,2,3,1
+INSERT INTO @Version(a, b, c, d)
+    SELECT 2,2,4,6
+INSERT INTO @Version(a, b, c, d)
+    SELECT 2,3,1,7
+INSERT INTO @Version(a, b, c, d)
+    SELECT 3,2,1,5
+
+select a,b,c,d from (
+select a,b,c,d,ROW_NUMBER() over (partition by a order by 
+b desc, c desc, d desc) from @Version)  -- use as many as features in feature
+as X(a,b,c,d,latest)
+where latest = 1
+declare @tbl_Orders table
+(
+	OrderID CHAR(5) 
+	,Step INTEGER 
+	,Status CHAR(1) 
+)
+
+ 
+INSERT INTO @tbl_Orders(OrderID, Step, Status)
+    SELECT 'ABC', 0, 'D'
+INSERT INTO @tbl_Orders(OrderID, Step, Status)
+	select 'ABC', 1, 'P'
+INSERT INTO @tbl_Orders(OrderID, Step, Status)
+	select 'ABC', 2, 'P'
+INSERT INTO @tbl_Orders(OrderID, Step, Status)
+	select 'ABC', 3, 'P'
+INSERT INTO @tbl_Orders(OrderID, Step, Status)
+	select 'XYZ', 0, 'P'
+INSERT INTO @tbl_Orders(OrderID, Step, Status)
+	select 'XYZ', 1, 'D'
+INSERT INTO @tbl_Orders(OrderID, Step, Status)
+	select 'EFQ', 0, 'D'
+INSERT INTO @tbl_Orders(OrderID, Step, Status)
+	select 'EFQ', 1, 'D'
+Go;
+
+-- generate the group of sequences. 
+;with cte as(
+select b.orderid, cast(rnk*10+c.step as varchar(3)) rnk from
+(select *, ROW_NUMBER()over (order by (select null)) rnk from 
+(select distinct orderid from @tbl_Orders) a) as  b
+inner join @tbl_Orders c on b.orderid = c.orderid
+)
+select distinct orderid, 
+STUFF ((SELECT ',' + rnk FROM CTE w WHERE w.orderid = t.orderid FOR XML PATH('')) ,1,1,'') SequenceNo_Groups
+FROM CTE t
+
+
+
